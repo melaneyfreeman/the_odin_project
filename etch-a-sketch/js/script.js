7b7909067065e9188233df8c271efb33eeac7806
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-06-13 17:36:42 -0600

    added comments and cleaned up code a bit

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,14 +1,19 @@
+//create container object
 const container = document.getElementById("container")
+
+//set up event listeners for all buttons
 document.getElementById("clearBtn").addEventListener("click", clear);
 document.getElementById("sizeBtn").addEventListener("click", changeSize);
 document.getElementById("blackPenBtn").addEventListener("click", blackPen);
 document.getElementById("rainbowPenBtn").addEventListener("click", rainbowPen);
 
+//create defualt grid when page loads
 createGrid(32);
 
 //clear when start so that it pays attention to the pen color
 clear();
 
+//takes size from input
 function createGrid(size){
     //removes the current grid, instead of adding onto it
     container.innerHTML = "";
@@ -22,7 +27,6 @@ function createGrid(size){
         frame.classList.add("frame-box");
         container.appendChild(frame);
     }
-  
 }
 
 function blackPen(){
@@ -36,7 +40,6 @@ function blackPen(){
     resetBtns();
     this.style.backgroundColor = "#f76a8c";
     this.style.color = "white";
-
 }
 
 function rainbowPen(){
@@ -50,10 +53,9 @@ function rainbowPen(){
     resetBtns();
     this.style.backgroundColor = "#f76a8c";
     this.style.color = "white";
-
-
 }
 
+//reset button styles so only active button remains highlighted
 function resetBtns(){
     btnArray = document.getElementsByTagName("button");
     for(let i = 0; i < btnArray.length; i++){
@@ -62,10 +64,12 @@ function resetBtns(){
     }
 }
 
+//create random color for rainbow pen
 function randomColor(){
     return Math.floor(Math.random() * 256);
 }
 
+//clears grid by removing classes that tell it has been hovered over/colored
 function clear(){
     const cells = document.querySelectorAll(".frame-box, .grid-box");
     Array.from(cells).forEach((element) => {
@@ -75,6 +79,7 @@ function clear(){
     });
 }
 
+//prompt to get custom grid size from user and clears the grid
 function changeSize(){
     var size = prompt("Enter new grid size!");
     createGrid(size);
