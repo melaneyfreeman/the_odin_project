c3cf89daf781b0d27335843af47a0e9948a7f500
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-07-09 16:57:59 -0600

    working on functionality with adding new books

--- library/js/library.js
+++ library/js/library.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+var myLibrary = [];
 let libraryWrapper = document.getElementById("wrapper");
 let newBookForm = document.getElementById("newBookFormContainer");
 
@@ -6,9 +6,16 @@ let newBookBtn = document.getElementById("addBookBtn");
 let closeFormBtn = document.getElementById("closeFormBtn");
 
 let deleteBookBtns = document.getElementsByClassName('deleteBookBtn');
+let addBookBtn = document.getElementById("submitBtn");
 
-myLibrary = document.getElementsByClassName('book');
-console.log(myLibrary);
+///
+let myLibraryBtns = document.getElementsByClassName('deleteBookBtn');
+
+let formTitle = document.getElementById("title");
+let formAuthor = document.getElementById("author");
+let formPages = document.getElementById("pages");
+
+let statusBtn = document.getElementsByClassName("statusBtn");
 
 //opening the menu to add a new book
 newBookBtn.onclick = function(){
@@ -19,22 +26,73 @@ closeFormBtn.onclick = function(){
     newBookForm.style.display = "none";
 }
 
+
+
+addBookBtn.onclick = function(){
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let status;
+    //get value for checked radio button for book satus
+    let radios = document.getElementsByName("bookStatus");
+    for(var i = 0; i < radios.length; i++){
+        if(radios[i].checked){
+            status = radios[i].value;
+            //since only one can be checked, dont check the rest
+            break;
+        }
+    }
+    
+    addBookToLibrary(title, author, pages, status);
+    newBookForm.style.display = "none";
+
+    return false;
+
+}
+
 //functionality to delete books by clicking on the "x" button
-for (var i = 0; i < myLibrary.length; i++){
+for (var i = 0; i < myLibraryBtns.length; i++){
     deleteBookBtns[i].addEventListener('click', function(e){
         e.currentTarget.parentNode.remove();
-    }, false);
+    }, true);
 }
 
 
+statusBtn.onclick = function(){
+    
+}
 
-
-
-function Book(){
-
+function Book(title, author, pages, status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
 
-function addBookToLibrary(){
+function addBookToLibrary(title, author, pages, status){
+    let book = new Book(title, author, pages, status);
+    
 
+    let clone = document.querySelector('#book').cloneNode(true);
+    clone.setAttribute('id', 'book');
+    clone.style.display = "block";
+    document.getElementById("wrapper").appendChild(clone);
+
+    clone.querySelector('H2').textContent = title;
+    clone.querySelector('H3').textContent = author;
+    clone.querySelector('H4').textContent = pages;
+    clone.querySelector('H5').textContent = status;
+
+
+    for (var i = 0; i < myLibraryBtns.length; i++){
+        deleteBookBtns[i].addEventListener('click', function(e){
+            e.currentTarget.parentNode.remove();
+        }, true);
+    }
+
+    //reset form values
+    formTitle.value = "";
+    formAuthor.value = "";
+    formPages.value = "";
 }
