5a57f0e5ba9a380d5d66c00340ecc0ae959a3d67
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-07-10 16:06:02 -0600

    issues with toggling and deleting

--- library/js/library.js
+++ library/js/library.js
@@ -8,14 +8,12 @@ let closeFormBtn = document.getElementById("closeFormBtn");
 let deleteBookBtns = document.getElementsByClassName('deleteBookBtn');
 let addBookBtn = document.getElementById("submitBtn");
 
-///
-
 let formTitle = document.getElementById("title");
 let formAuthor = document.getElementById("author");
 let formPages = document.getElementById("pages");
 
-let changeStatusBtn = document.getElementsByClassName("changeStatusBtn");
 let statusText = document.getElementById("statusText");
+
 
 //opening the menu to add a new book
 newBookBtn.onclick = function(){
@@ -26,7 +24,6 @@ closeFormBtn.onclick = function(){
     newBookForm.style.display = "none";
 }
 
-init();
 
 function init(){
     addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", "423", "read");
@@ -36,8 +33,6 @@ function init(){
     addBookToLibrary("Catching Fire", "Suzanne Collins", "391", "read");
     addBookToLibrary("Mockingjay", "Suzanne Collins", "390", "read");
 }
-
-
 
 addBookBtn.onclick = function(){
     let title = document.getElementById("title").value;
@@ -49,7 +44,7 @@ addBookBtn.onclick = function(){
     for(var i = 0; i < radios.length; i++){
         if(radios[i].checked){
             status = radios[i].value;
-            //since only one can be checked, dont check the rest
+            //since only one can be checked, don't check the rest
             break;
         }
     }
@@ -60,33 +55,42 @@ addBookBtn.onclick = function(){
     return false;
 
 }
-
-libraryWrapper.addEventListener('click', deleteOrToggle);
-
-function deleteOrToggle(e){
-    //functionality to delete books by clicking on the "x" button
+//functionality to delete books by clicking on the "x" button
+function deleteBook(){
     const del = Array.from(document.getElementsByClassName("deleteBookBtn"));
     del.forEach(function(button, index){
         button.addEventListener("click", function(e){
-            myLibrary.splice(index-1, 1);
+            myLibrary.splice(index, 1);
             e.currentTarget.parentNode.remove();
         });
     });
-
+}
+ //functionality to toggle status
+function toggleStatus(book){
     const change = Array.from(document.getElementsByClassName("changeStatusBtn"));
     change.forEach(function(button, index){
         button.addEventListener("click", function(e){
-            if(e.target.innerHTML === "read"){
-                button.innerHTML = "unread";
+            change.id = index;
+            if(e.currentTarget.innerHTML === "unread"){
+                e.currentTarget.style.backgroundColor = "yellow";
+                e.currentTarget.innerHTML = "read";
+                book.status = "read";
+                //e.currentTarget.parentNode.querySelector('H5').textContent = "read"; 
+                //myLibrary[index].status = "read";      
             }
             else{
-                button.innerHTML = "read";
+                e.currentTarget.style.color = "red";
+                e.currentTarget.innerHTML = "unread";
+                book.status = "unread";
+
+               // e.currentTarget.parentNode.querySelector('H5').textContent = "unread";
+               //myLibrary[index].status = "unread";        
             }
-      });
+         });
     });
-}
+} 
 
-
+//book object
 function Book(title, author, pages, status){
     this.title = title;
     this.author = author;
@@ -94,6 +98,7 @@ function Book(title, author, pages, status){
     this.status = status;
 }
 
+//add book to array and fill out book elements
 function addBookToLibrary(title, author, pages, status){
     let book = new Book(title, author, pages, status);
     myLibrary.push(book);
@@ -109,32 +114,14 @@ function addBookToLibrary(title, author, pages, status){
     clone.querySelector('H4').textContent = pages;
     clone.querySelector('.changeStatusBtn').textContent = status;
 
-
-  //functionality to delete books by clicking on the "x" button
-    const del = Array.from(document.getElementsByClassName("deleteBookBtn"));
-    del.forEach(function(button, index){
-        button.addEventListener("click", function(e){
-            myLibrary.splice(index-1, 1);
-            e.currentTarget.parentNode.remove();
-        });
-    });
-
-    const change = Array.from(document.getElementsByClassName("changeStatusBtn"));
-    change.forEach(function(button, index){
-        button.addEventListener("click", function(e){
-            if(button.innerHTML === "read"){
-                button.innerHTML = "unread";
-                myLibrary[index].status = "unread";
-            }
-            else{
-                button.innerHTML = "read";
-                myLibrary[index].status = "read";
-            }
-      });
-    });
-
     //reset form values
     formTitle.value = "";
     formAuthor.value = "";
-    formPages.value = "";
+    formPages.value = ""; 
+
+    //event listeners
+    deleteBook();
+    toggleStatus(this.book);
 }
+
+
