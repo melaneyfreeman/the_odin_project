fe82793e18077dfb33c2d871e3cab9add1ed98ca
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-07-23 16:28:43 -0600

    working on reducing amount of global code

--- tic-tac-toe/js/tic-tac-toe.js
+++ tic-tac-toe/js/tic-tac-toe.js
@@ -4,41 +4,71 @@ let isPlayer1Turn = true;
 
 console.log(gameBoardTiles);
 
-//all the different combinations you can win with in relation to the gameBoard array
-const winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-
 //event listener for the tiles
-for(let i = 0; i < gameBoardTiles.length; i++){
-    gameBoardTiles[i].addEventListener("click", function(){
-        play(gameBoardTiles[i], i);
-    });
+function addTileEventListener(){
+    for(let i = 0; i < gameBoardTiles.length; i++){
+        gameBoardTiles[i].addEventListener("click", function(){
+            play(gameBoardTiles[i], i);
+        });
+    }
 }
 
+addTileEventListener();
+
 //game board as an array
-let gameBoard = ["", "", "",
-                 "", "", "",
-                 "", "", ""];
+//uses "-" as placeholders so the regex works properly
+//instead of just ""
+let gameBoard = ["-", "-", "-",
+                 "-", "-", "-",
+                 "-", "-", "-"];
 
 
-function Player(name, marker){
+function Player(name, marker, wins, loses){
     this.name = name;
     this.marker = marker;
+    this.wins  = wins;
+    this.loses = loses;
 }
 
 function play(i, index){
     console.log("you clicked " + i);
-    if(isPlayer1Turn && gameBoard[index] === ""){
-        gameBoard[index] = "X";
+    if(isPlayer1Turn && gameBoard[index] === "-"){
+        gameBoard[index] = "x";
         console.log(gameBoard);
         i.innerHTML = "x";
         isPlayer1Turn = false;
     
     }
-    else if (!isPlayer1Turn && gameBoard[index] === ""){
-        gameBoard[index] = "O";
+    else if (!isPlayer1Turn && gameBoard[index] === "-"){
+        gameBoard[index] = "o";
         console.log(gameBoard);
         i.innerHTML = "o";
         isPlayer1Turn = true;
     }
 
+    checkWin();
 }
+
+function checkWin(){
+    //turn gameboard to string
+    let str = gameBoard.join("");
+    console.log(str);
+
+    //regex, checking for all possible combinations to win
+    if(/x..x..x|...xxx...|xxx......|......xxx|x...x...x|..x.x.x../gi.test(str)){
+        console.log("x win");
+        
+    }
+
+    else if(/o..o..o|...ooo...|ooo......|......ooo|o...o...o|..o.o.o../gi.test(str)){
+        console.log("o win");
+    }
+
+    else{
+        console.log("tie");
+    }
+
+
+}
+
+
