e7ffb35ca5f839a8798de2e402574e25bb48c7fb
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-09-24 19:10:17 -0600

    working on webpack tutorial, got loading fonts working. next is loading data

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path');
 
-module.exports = {
+  module.exports = {
     entry: './src/index.js',
-    output:{
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-};
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+
+       {
+
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+
+         use: [
+
+           'file-loader',
+
+         ],
+
+       },
+      ],
+    },
+  };
