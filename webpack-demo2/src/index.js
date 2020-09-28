d8e0194060fe04248e2e235dfa706c7a3440f797
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-09-28 16:13:40 -0600

    added progress file so i can keep track of where i am on lessons if i lose my place

--- webpack-demo2/src/index.js
+++ webpack-demo2/src/index.js
@@ -1,25 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './toky.png';
-import Data from './data.xml'
+import printMe from './print.js';
 
   function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-   // Add the image to our existing div.
+    btn.innerHTML = 'click me and check the console!';
+    btn.onclick = printMe;
 
-   const myIcon = new Image();
-
-   myIcon.src = Icon;
-
-
-   element.appendChild(myIcon);
-
-   console.log(Data);
+    element.appendChild(btn);
 
     return element;
   }
--- webpack-demo2/src/myName.js
+++
@@ -1,2 +0,0 @@
-const myName = (name) => "Hi! My name is " + name;
-export default myName;
