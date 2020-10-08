937d21e89bda2059d1d14f673061a3ca1a8d946f
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-10-08 11:36:29 -0600

    working on TOP assignment for working on handling inputs and render lists

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      task: "",
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  };
+
+  onSubmitTask = (e) => {
+    //call this because we don't want the default behavior
+    //of refreshing the form anytime we submit it
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+    });
+  };
+
+  render(){
+    const{task, tasks} = this.state;
+
+    return (
+    
+      <div>
+        <div>
+          <form>
+            <label htmlFor="taskInput">Enter a task:</label>
+            <input type="text" id="taskInput"></input>
+          </form>
+        </div>
+
+        <button type="submit">Add task</button>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
