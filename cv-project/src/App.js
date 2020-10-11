6f8bac3d48993edd3e3f67c40505ec8985a14a2e
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-10-11 12:04:16 -0600

    working on having multiple educations functionality

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import './components/General';
 import General from './components/General';
+import Education from './components/Education'
 
 class App extends Component {
   constructor(){
     super();
 
     this.state = {
-      isEditingGen: true
+      isEditingGen: false,
+      isEditingEd: false,
+      educationCount: 1
     };
   
   }
@@ -17,12 +20,30 @@ class App extends Component {
     this.setState({
       isEditingGen: !this.state.isEditingGen
     });
-    console.log(this.state.isEditingGen)
+    console.log("gen edit: " + this.state.isEditingGen)
+  }
+
+  onEducationEdit(){
+    this.setState({
+      isEditingEd: !this.state.isEditingEd
+    });
+    console.log("ed edit: " + this.state.isEditingEd)
+  }
+
+  onEducationAdd(){
+    this.setState({
+      educationCount: this.state.educationCount + 1
+    })
   }
 
   
   render(){ 
     let general = "";
+    let education = "";
+    let educations = [];
+
+    //editing general information handling
+    //if editing general
     if(this.state.isEditingGen){
       general = (
         <div>
@@ -32,18 +53,44 @@ class App extends Component {
       );
     }
 
+    //if not editing general
     else{
       general = (
         <div>
           <General isEditing={false}/>
-          <button onClick={this.onGeneralEdit.bind(this)}>edit</button>
+          <button onClick={this.onGeneralEdit.bind(this)}>edit general</button>
+
       </div>
       );
     }
+
+    //editing education information handling
+    //if editing education
+    if(this.state.isEditingEd){
+      education = (
+        <div>
+          <Education isEditing={true}/>
+          <button onClick={this.onEducationEdit.bind(this)}>submit</button>
+
+        </div>
+      )
+    }
+
+    else{
+      education = (
+        <div>
+          <Education isEditing={false}/>
+          <button onClick={this.onEducationEdit.bind(this)}>edit education</button>
+        </div>
+      )
+    }
+
     
     return(
       <div className="App">
         {general}
+       
+        {education}
       </div>
     )
  
---
+++ cv-project/src/components/Education.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+class Education extends React.Component{
+    constructor(props){
+        super();
+
+        this.state = {
+            isEditing: props.isEditing,
+            school: "school",
+            degree: "bachelors of school",
+            minor: "minor study",
+            graduationYear: "2019"
+        }
+    }
+
+    handleInputChange = ({
+        target: {name, value}}) => {
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render(){
+        if(this.props.isEditing){
+            return(
+                <div>
+                <label>school:</label>
+                <input  className="school" 
+                        name="school"
+                        type="text"
+                        value={this.state.school}
+                        onChange={this.handleInputChange}>
+                </input> 
+
+                <label>degree:</label>
+                <input  className="degree" 
+                        name="degree"
+                        type="text"
+                        value={this.state.degree}
+                        onChange={this.handleInputChange}></input>
+
+                <br></br>
+                <br></br>
+
+                <label>(optional) minor:</label>
+                <input  className="minor" 
+                        name="minor"
+                        type="text"
+                        value={this.state.minor}
+                        onChange={this.handleInputChange}></input> 
+
+                <label>graduation year:</label>
+                <input  className="graduationYear" 
+                        name="graduationYear"
+                        type="number"
+                        max="3000"
+                        value={this.state.graduationYear}
+                        onChange={this.handleInputChange}></input>
+                <br></br>
+                <br></br>
+
+                </div>
+            )
+        }
+        else{
+            if(this.state.minor) {
+                return(
+                    <div>
+                        <h2>Education</h2>
+                        <h3>school</h3>
+                        <p>{this.state.school}</p>
+                        <h3>Degree/Area of Study</h3>
+                        <p>{this.state.degree}</p>
+                        <h3>Minor</h3>
+                        <p>{this.state.minor}</p>
+                        <h3>Graduation</h3>
+                        <p>{this.state.graduationYear}</p>
+                    </div>                
+                )
+            }
+
+            else{
+            return(
+                    <div>
+                        <h2>Education</h2>
+                        <h3>school</h3>
+                        <p>{this.state.school}</p>
+                        <h3>Degree/Area of Study</h3>
+                        <p>{this.state.degree}</p>
+                        <h3>Graduation</h3>
+                        <p>{this.state.graduationYear}</p>
+                    </div>                
+                )
+            }
+        }
+
+     
+
+       
+    }
+}
+
+export default Education;
