9159e260c8935aa5e7cd9080dc4a7934523a6e58
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2020-11-26 11:55:59 -0700

    typo on last commit, changing from function components to class components. completed this. next will work on getting the cart to update

--- shopping_cart/src/Components/Shop.js
+++ shopping_cart/src/Components/Shop.js
@@ -1,63 +1,75 @@
-import React, { useState, useEffect } from 'react'
+import {React, Component } from 'react'
 import { Link } from 'react-router-dom'
 import Cart from './Cart'
 
-function Shop() {
+class Shop extends Component {
+    constructor(){
+        super()
 
-    useEffect(() => {
-        fetchItems()
-    }, [])
+        this.state = {
+            items: []
+        }
+    }
 
-    const [items, setItems] = useState([])
+    //when the page loads, immediately fetch the data
+    componentDidMount(){
+        this.fetchItems()
+    }
 
-    const fetchItems = async () => {
+    fetchItems = async () => {
         const data = await fetch('https://ffxivcollect.com/api/minions?id_in=80..110')
         const items = await data.json();
         console.log(items.results)
-        setItems(items.results)
+        this.setState({
+            items: items.results
+        })
     }
 
-    return (
-        <div>
-        <Cart/>
-            <h1>Shop</h1>
-            <div className="shop-div">
-                {items.map(item => (
-                    <div key={item.id}>
-                        <h2>
-                            <Link to={`/shop/${item.id}`} className="minion-links">
-                                <img src={item.image} alt={item.name + ' minion'} className="shop-img" />
-                                <br></br>
-                                {item.name}
-                            </Link>
-                        </h2>
-
-                        {/*I'm  using the patch as the price points, since they are not
-                        included in the API if they are MogStation items. I check if the patch
-                        length is smaller than 4 characters to know if I just append a 0 to the end
-                        to make it appear as if it is just a dollar value, for example...
-                        an item is from patch 3.1, I will add a 0 to make it $3.10 */}
-
-                        {item.patch.length < 4 &&
-                            <div>
-                                <h4>${item.patch}0</h4>
-                            </div>
-                        }
-
-                        {item.patch.length > 3 &&
-                            <div>
-                                <h4>${item.patch}</h4>
-                                
-                            </div>
-                        }
-
-                    </div>
-                ))}
-
-
+    render(){
+        return (
+            <div>
+            <Cart/>
+                <h1>Shop</h1>
+                <div className="shop-div">
+                    {this.state.items.map(item => (
+                        <div key={item.id}>
+                            <h2>
+                                <Link to={`/shop/${item.id}`} className="minion-links">
+                                    <img src={item.image} alt={item.name + ' minion'} className="shop-img" />
+                                    <br></br>
+                                    {item.name}
+                                </Link>
+                            </h2>
+    
+                            {/*I'm  using the patch as the price points, since they are not
+                            included in the API if they are MogStation items. I check if the patch
+                            length is smaller than 4 characters to know if I just append a 0 to the end
+                            to make it appear as if it is just a dollar value, for example...
+                            an item is from patch 3.1, I will add a 0 to make it $3.10 */}
+    
+                            {item.patch.length < 4 &&
+                                <div>
+                                    <h4>${item.patch}0</h4>
+                                </div>
+                            }
+    
+                            {item.patch.length > 3 &&
+                                <div>
+                                    <h4>${item.patch}</h4>
+                                    
+                                </div>
+                            }
+    
+                        </div>
+                    ))}
+    
+    
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    
 }
 
 export default Shop
--- shopping_cart/src/Components/ShopItem.js
+++ shopping_cart/src/Components/ShopItem.js
@@ -1,53 +1,57 @@
-import { createEvent } from '@testing-library/react';
-import React, { useState, useEffect } from 'react'
+
+import {React, Component } from 'react'
 import Cart from './Cart'
 
 
-function ShopItem({ match }) {
-    useEffect(() => {
-        fetchItem();
-    }, [])
+class ShopItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            results: {
+                name:{
 
-    let cart = (<Cart/>)
-
-    const [itemDetail, setItemDetail] = useState({
-        results: {
-            name: {
-
-            }
+                }
+            },
+            itemId: props.match.params.itemId,
+            itemDetail: ""
         }
-    })
+    }
+
+    //when the page loads, immediately fetch the data
+    componentDidMount(){
+        this.fetchItem()
+    }
 
 
-    const fetchItem = async () => {
-        const fetchItem = await fetch(`https://ffxivcollect.com/api/minions?id_in=${match.params.id}`)
+    fetchItem = async () => {
+        const fetchItem = await fetch(`https://ffxivcollect.com/api/minions?id_in=${this.state.itemId}`)
         const itemDetail = await fetchItem.json()
-        setItemDetail(itemDetail.results[0])
+        this.setState({
+            itemDetail: itemDetail.results[0]
+        })
         console.log(itemDetail.results[0])
     }
 
-    function addToCart(id){
+    addToCart(id) {
         console.log("button clicked" + id)
-        cart = (
-            <Cart item={id}/>
+       
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="shop-item">
+                    <h1>{this.state.itemDetail.name}</h1>
+                    <h2>{this.state.itemDetail.description}</h2>
+                    <h2>{this.state.itemDetail.enhanced_description}</h2>
+                    <img src={this.state.itemDetail.image} alt={this.state.itemDetail.name}></img>
+                    <h5>{this.state.itemDetail.tooltip}</h5>
+                    <h4>Price: {this.state.itemDetail.patch}</h4>
+                    <button id={this.state.itemDetail.id} onClick={e => this.addToCart(e.target.id)}>add to cart</button>
+                </div>
+            </div>
         )
     }
-    
-
-    return (
-        <div>
-        {cart}
-        <div className="shop-item">
-            <h1>{itemDetail.name}</h1>
-            <h2>{itemDetail.description}</h2>
-            <h2>{itemDetail.enhanced_description}</h2>
-            <img src={itemDetail.image} alt={itemDetail.name}></img>
-            <h5>{itemDetail.tooltip}</h5>
-            <h4>Price: {itemDetail.patch}</h4>
-            <button id={itemDetail.id} onClick={e => addToCart(e.target.id)}>add to cart</button>
-        </div>
-        </div>
-    )
 }
 
 export default ShopItem
