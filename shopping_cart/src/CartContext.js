6edd91d0adb14c446932c6aa432453a566075f24
Melaney Freeman <melaneyfreeman@u.boisestate.edu>
2021-05-31 13:28:10 -0600

    working on getting back into coding (again), sleeping issues still a problem but i  have medication whoo. trying to get back into it. working on changing the item page to instead a popup window when you click details instead of having two instances of a add to cart button where one doesnt work. should smooth out the issue i think

--- shopping_cart/src/CartContext.js
+++ shopping_cart/src/CartContext.js
@@ -14,6 +14,46 @@ export const CartProvider = (props) => {
     console.log("cart items from cart context")
     console.log(cartItems)
 
+    //one way of checking for duplicates
+/*     function checkDouble(){
+        var arr = cartItems
+        for (let i = 0; i < arr.length; i++){
+            for(let x = 0; x < arr.length; x++){
+                if(arr[i] === arr[x] && i !== x){
+                    console.log("SAME ONES IN ARRAY!!!!!!!!!" + arr[i])
+                }
+                else
+                    console.log("no duplicates whoo")
+            }
+        }
+    }
+
+    checkDouble() */
+
+    //other way?
+        function getUnique(arr, index){
+            const unique = arr
+                .map(e=> e[index])
+
+                //store the keys of the unique objects
+                .map((e, i, final) => final.indexOf(e) === i && i)
+
+                //eliminate the dead keys & store unique objects
+                .filter(e => arr[e]).map(e => arr[e]);
+
+                return unique
+        }
+        
+
+        console.log(getUnique(cartItems, 'id') + "should be no doubles????")
+
+        //this causes too many rerenders?
+        //setCartItems(getUnique(cartItems, 'id'))
+
+        
+
+
+
     return (
         <CartContext.Provider value={[cartItems, setCartItems]}>
             {props.children}
